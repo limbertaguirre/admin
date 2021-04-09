e51effacde2b81b387a97a855b6de215ed9dbd50
Limbert Aguirre <laguirre@gruposion.bo>
2021-04-09 10:24:04 -0400

    pakage

--- gestion_de_comisiones/ClientApp/src/components/NavMenu.js
+++ gestion_de_comisiones/ClientApp/src/components/NavMenu.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -26,7 +43,8 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">gestion_de_comisiones</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">gestion_de_comisiones</NavbarBrand> 
+
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
@@ -41,6 +59,7 @@ export class NavMenu extends Component {
                 </NavItem>
               </ul>
             </Collapse>
+
           </Container>
         </Navbar>
       </header>
