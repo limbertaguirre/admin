74e267c34d70380ce37961933aef04ce33f246ba
Limbert Aguirre <laguirre@gruposion.bo>
2021-04-09 16:44:11 -0400

    inicializando loading

--- gestion_de_comisiones/ClientApp/src/redux/reducers/rootReduces.js
+++ gestion_de_comisiones/ClientApp/src/redux/reducers/rootReduces.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import messageReducer from './messageReducer';
+import loginReducer from './loadingReducer';
 import homeReducer from '../reducers/homeReducer';
 
 const rootReducer = combineReducers({  
      message:messageReducer,
+     load:loginReducer,
      home:homeReducer, 
 });
 
