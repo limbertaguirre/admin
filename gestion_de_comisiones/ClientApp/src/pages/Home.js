d9b925273e67a41170aac084e10c5476fe5f6e4c
Limbert Aguirre <laguirre@gruposion.bo>
2021-04-19 12:04:06 -0400

    home migas de pan

--- gestion_de_comisiones/ClientApp/src/pages/Home.js
+++ gestion_de_comisiones/ClientApp/src/pages/Home.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import {Breadcrumbs,Chip,emphasize, withStyles} from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
 import {useSelector, useDispatch} from "react-redux";
 import * as Action from '../redux/actions/homeAction';
+
+const StyledBreadcrumb = withStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.grey[100],
+    height: theme.spacing(3),
+    color: theme.palette.grey[800],
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: theme.palette.grey[300],
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(theme.palette.grey[300], 0.12),
+    },
+  },
+}))(Chip); 
+
+
 
 export const Home =()=> {
 
@@ -10,6 +30,11 @@ export const Home =()=> {
      
     return (
       <>
+        <Breadcrumbs aria-label="breadcrumb">
+              <StyledBreadcrumb key={1} component="a" label="Home"icon={<HomeIcon fontSize="small" />}  />
+              
+        </Breadcrumbs>
+        <br />
         <h1>Hello, que hace!</h1>
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
--- gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/CargarComisiones.js
+++ gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/CargarComisiones.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import {Breadcrumbs,Chip,emphasize, withStyles} from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
+import { Breadcrumbs, Chip, emphasize, withStyles } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
 
 const StyledBreadcrumb = withStyles((theme) => ({
   root: {
@@ -9,36 +9,34 @@ const StyledBreadcrumb = withStyles((theme) => ({
     height: theme.spacing(3),
     color: theme.palette.grey[800],
     fontWeight: theme.typography.fontWeightRegular,
-    '&:hover, &:focus': {
+    "&:hover, &:focus": {
       backgroundColor: theme.palette.grey[300],
     },
-    '&:active': {
+    "&:active": {
       boxShadow: theme.shadows[1],
       backgroundColor: emphasize(theme.palette.grey[300], 0.12),
     },
   },
-}))(Chip); 
+}))(Chip);
 
-const CargarComisiones =()=> {
-  
-    
+const CargarComisiones = () => {
+  return (
+    <>
+      <Breadcrumbs aria-label="breadcrumb">
+        <StyledBreadcrumb
+          key={1}
+          component="a"
+          label="Gestion de pagos"
+          icon={<HomeIcon fontSize="small" />}
+        />
+        <StyledBreadcrumb key={2} component="a" label="Pago de comisiones" />
+        <StyledBreadcrumb key={3} label="Cargar Comisiones" />
+      </Breadcrumbs>
+      <br />
+      <h1>Cargar Comisiones</h1>
 
-
-    return (
-      <>
-           <Breadcrumbs aria-label="breadcrumb">
-                    <StyledBreadcrumb key={1} component="a" label="Gestion de pagos"icon={<HomeIcon fontSize="small" />}  />
-                    <StyledBreadcrumb key={2} component="a" label="Pago de comisiones"  />
-                    <StyledBreadcrumb key={3} label="Prorrateo" />
-           </Breadcrumbs>
-        <br />
-        <h1>Cargar Comisiones</h1>
-
-        <p>This is a simple example of a React component.</p>
-
-        
-      </>
-    );
-  
-}
+      <p>This is a simple example of a React component.</p>
+    </>
+  );
+};
 export default CargarComisiones;
