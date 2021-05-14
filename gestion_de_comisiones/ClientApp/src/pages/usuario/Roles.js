1f56395932967ff63e815403e301a43ff12000db
laguirre <laguirre@gruposion.bo>
2021-05-14 15:57:02 -0400

    agregando componente

--- gestion_de_comisiones/ClientApp/src/pages/usuario/Roles.js
+++ gestion_de_comisiones/ClientApp/src/pages/usuario/Roles.js
@@ -1,16 +1,83 @@
 
 
-import React  from 'react';
-import Typography from '@material-ui/core/Typography';
+import React,{useState}  from 'react';
+import { TextField, Typography, InputAdornment } from "@material-ui/core";
+import { Dialog, DialogContent, Button, Grid } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+import { verificaAlfanumerico } from "../../lib/expresiones";
 
+const useStyles = makeStyles((theme) => ({
+    icono: {
+        width: '40px',
+        height: '40px',
+    },
+    contentTitle: {
+        textAlign: 'rigth',
+    },
+    contentButton: {
+        textAlign: 'center',
+    },
+    contentDialog: {
+         background: '#bgfg55',
+        width: '70%',
+    },
+    TextFiel: {
+        marginBottom: theme.spacing(1),
+        marginTop: theme.spacing(1),
+        width: '50%',
+    },
+    formControl: {
+        marginBottom: theme.spacing(1),
+        marginTop: theme.spacing(1),
+    },
 
+}));
 
 const  Roles =()=>  {       
+    const style = useStyles();
+    const [rolName, setRolName] = useState("");
+    const [rolNameError, setRolNameError] = useState(false);
+ 
+    const isValidRolName = (usuarioName) => {   
+        return  usuarioName.length >= 5 && verificaAlfanumerico(usuarioName);
+    };
+    const _onChangeregistro= (e) => {
+        const texfiel = e.target.name;
+        const value = e.target.value;
+        if (texfiel === "rolName") {
+            setRolName(value);
+            setRolNameError(!isValidRolName(value));
+        }
+    };
+
+
     return (
          <>            
             <Typography variant="h6" gutterBottom>
                 GESTION DE ROLES
-            </Typography>         
+            </Typography>   
+            <Grid item xs={12} className={style.contentTitle} >
+                         <TextField
+                            id="rolName"
+                            label="Nuevo Rol"
+                            type={'text'}
+                            variant="outlined"
+                            name="rolName"
+                            value={rolName}
+                            onChange={_onChangeregistro}
+                            className={style.TextFiel}
+                            error={rolNameError}
+                            helperText={ rolNameError &&
+                            "El campo es requerido"
+                            }
+                            required
+                            //fullWidth
+                            inputProps={{
+                            maxLength: 20,
+                            }}
+                        />      
+
+           </Grid>
             <br />     
          </>
     );
