4209604c6693db92893801627edcfa7e2f2fde91
Marwin Germán Núñez Robles <mgnunez@gruposion.bo>
2021-10-28 11:49:56 -0400

    Rebase final.

--- gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
+++ gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
@@ -35,6 +35,8 @@ import SearchIcon from "@material-ui/icons/Search";
 import * as Actions from "../../../redux/actions/PagosGestorAction";
 import * as ActionMensaje from "../../../redux/actions/messageAction";
 
+import GridTransferencia from "./Components/GridTransferencia";
+
 const StyledBreadcrumb = withStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.grey[100],
@@ -184,9 +186,6 @@ const Pagos = (props) => {
         "¡Debe Seleccionar un ciclo para cargar las comisiones!",
         "warning"
       );
-      /*  setOpenSnackbar(true);
-          setMensajeSnackbar('¡Debe Seleccionar un ciclo para cargar las comisiones!');
-          settipTSnackbar('warning'); */
     }
   };
 
@@ -238,7 +237,6 @@ const Pagos = (props) => {
       console.log("busqueda por filtro", response);
       if (response && response.code == 0) {
         let data = response.data;
-        // setPendienteFormaPago(data.pendienteFormaPago);
         setListaComisionesAPagar(data.lista);
       }
     } else {
@@ -266,7 +264,6 @@ const Pagos = (props) => {
     if (response && response.code == 0) {
       console.log("response busca ", response);
       let data = response.data;
-      // setPendienteFormaPago(data.pendienteFormaPago);
       setListaComisionesAPagar(data);
     }
   }
@@ -337,8 +334,7 @@ const Pagos = (props) => {
                       variant="contained"
                       color="secondary"
                       className={style.submitSAVE}
-                      // onClick = {()=> verificarConfirmarFomaPago()}
-                      onClick={() => abrirTransferenciaModal()}
+                      href="GridTransferencia"
                     >
                       PAGAR POR TRANSFERENCIA
                     </Button>
