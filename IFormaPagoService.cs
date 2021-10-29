defe90f9e9690e3c33fbb47dc922a9bb42892864
Eliot Humerez <ehumerez@gruposion.bo>
2021-10-29 17:10:11 -0400

    Bajando cambios develop/origin

---
+++ IFormaPagoService.cs
@@ -0,0 +1,24 @@
+﻿using gestion_de_comisiones.Modelos.Factura;
+using gestion_de_comisiones.Modelos.FormaPago;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gestion_de_comisiones.Servicios.Interfaces
+{
+    public interface IFormaPagoService
+    {
+        public object GetCiclos(string usuario);
+        public object GetFormasPagosPendientes(string usuario, int idCiclo);
+        public object ListarFormasPagos(ParamFormaPagosOutputModel param);
+        public object AplicarMetodoPago(AplicarMetodoOutput param);
+        public object ListarComisionesFormaPagoPorCarnet(BuscarInputModel param);
+        public object GetFormaPagosDisponibles(FormaPagosDisponiblesInputModel param);
+        public object FiltrarComisionesPorTipoPago(FiltroComisionTipoPagoInputModel param);
+        public object VerificarAutorizadorPorComision(AutorizacionVerificarParam param);
+        public object ConfirmarAutorizacionPagos(ConfirmarAutorizacionParam param);
+        public object VerificarCierreFormaPago(VerificarCierreFormaPagoParam param);
+        public object CerrarFormaDePago(CierreformaPagoInput param);
+    }
+}
