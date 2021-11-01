9b660f75038bc98dc96ad4e03b2500227ee48ef3
Eliot Humerez <ehumerez@gruposion.bo>
2021-11-01 10:32:14 -0400

    Anadiendo al script una nueva vista para listar empresas

--- script/SCRIPT_BD_COMISIONES_MULTINIVEL.sql
+++ script/SCRIPT_BD_COMISIONES_MULTINIVEL.sql
@@ -1566,3 +1566,22 @@ and c.id_tipo_comision = 1
 and l.id_lista_formas_pago not in (select dl.id_lista_formas_pago from BDMultinivel.dbo.GP_DETALLE_ESTADO_LISTADO_FORMA_PAGOL dl where dl.habilitado = 1 and dl.id_estado_listado_forma_pago = 1)
 group by l.id_comisiones_detalle, f.codigo_cnx, f.cuenta_bancaria, c.id_ciclo, f.nombres, f.apellidos, f.ci, l.monto_neto, cde.id_empresa, e.nombre, ci.nombre, l.id_tipo_pago
 GO
+
+CREATE view [dbo].[vwObtenerEmpresasComisionesDetalleEmpresa]
+as
+select c.id_ciclo
+, cde.id_empresa
+, TRIM(e.nombre) as empresa
+, c.id_tipo_comision
+, l.id_tipo_pago
+, sum(cde.monto_neto) monto_transferir
+from LISTADO_FORMAS_PAGO l
+inner join GP_COMISION_DETALLE cd on cd.id_comision_detalle = l.id_comisiones_detalle
+inner join GP_COMISION c on c.id_comision = cd.id_comision
+inner join BDMultinivel.dbo.COMISION_DETALLE_EMPRESA cde on cde.id_comision_detalle = cd.id_comision_detalle
+inner join BDMultinivel.dbo.EMPRESA e on e.id_empresa = cde.id_empresa
+where l.monto_neto <> 0
+and l.id_lista_formas_pago not in (select dl.id_lista_formas_pago from BDMultinivel.dbo.GP_DETALLE_ESTADO_LISTADO_FORMA_PAGOL dl where dl.habilitado = 1 and dl.id_estado_listado_forma_pago = 1)
+group by c.id_ciclo, cde.id_empresa, e.nombre , c.id_tipo_comision
+, l.id_tipo_pago
+GO
