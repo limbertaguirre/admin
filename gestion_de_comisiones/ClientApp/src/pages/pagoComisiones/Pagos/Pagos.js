8109fd162677e2725850c9fe62c3fb7fe8a94e44
Marwin Germán Núñez Robles <mgnunez@gruposion.bo>
2021-11-01 10:05:51 -0400

    Se prepara los pasos para realizar merge

--- gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
+++ gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
@@ -35,7 +35,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import * as Actions from "../../../redux/actions/PagosGestorAction";
 import * as ActionMensaje from "../../../redux/actions/messageAction";
 
-import GridTransferenciaModal from "./Components/GridTransferencia";
+// import GridTransferenciaModal from "./Components/GridTransferencia";
 
 const StyledBreadcrumb = withStyles((theme) => ({
   root: {
@@ -300,13 +300,13 @@ dispatch(ActionMensaje.showMessage({ message: response.message , variant: "error
 
     const [openModalFullScreen, setOpenModalFullScreen] = useState(false);
 
-  const openFullScreenModal = () => {
-    setOpenModalFullScreen(true);
-  };
+  // const openFullScreenModal = () => {
+  //   setOpenModalFullScreen(true);
+  // };
 
-  const closeFullScreenModal = () => {
-    setOpenModalFullScreen(false);
-  };
+  // const closeFullScreenModal = () => {
+  //   setOpenModalFullScreen(false);
+  // };
 
 
     return (
@@ -344,7 +344,7 @@ dispatch(ActionMensaje.showMessage({ message: response.message , variant: "error
                       variant="contained"
                       color="secondary"
                       className={style.submitSAVE}
-                      onClick = {() => openFullScreenModal()}
+                      //onClick = {() => openFullScreenModal()}
                     >
                       PAGAR POR TRANSFERENCIA
                     </Button>
@@ -484,7 +484,7 @@ dispatch(ActionMensaje.showMessage({ message: response.message , variant: "error
         handleCloseConfirm={confirmarModal}
         handleCloseCancel={CloseModalConfirmacion}
       />
-      <GridTransferenciaModal openModalFullScreen = {openModalFullScreen} closeFullScreenModal = {closeFullScreenModal}/>
+      {/* <GridTransferenciaModal openModalFullScreen = {openModalFullScreen} closeFullScreenModal = {closeFullScreenModal}/> */}
     </>
   );
 };
