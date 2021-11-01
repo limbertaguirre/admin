493d2cf4afefebbcc5c2e9685effd47501e8dc46
Marwin Germán Núñez Robles <mgnunez@gruposion.bo>
2021-11-01 11:55:34 -0400

    Agregamos Grilla

--- gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
+++ gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
@@ -1,25 +1,41 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import SnackbarSion from "../../../components/message/SnackbarSion";
-import * as permiso from '../../../routes/permiso'; 
-import { verificarAcceso, validarPermiso} from '../../../lib/accesosPerfiles';
-import MessageConfirm from '../../../components/mesageModal/MessageConfirm';
+import * as permiso from "../../../routes/permiso";
+import { verificarAcceso, validarPermiso } from "../../../lib/accesosPerfiles";
+import MessageConfirm from "../../../components/mesageModal/MessageConfirm";
 
-import GridPagos from './Components/GridPagos'
-import {Container,Tooltip ,Zoom, Chip, InputAdornment,Card, Button,
-  Grid, TextField, Typography,FormControl,
-   InputLabel, Select,MenuItem, Breadcrumbs } from "@material-ui/core";
-import HomeIcon from '@material-ui/icons/Home';
-import {emphasize, withStyles} from '@material-ui/core';
-import {  makeStyles } from '@material-ui/core/styles';
-import { useDispatch, useSelector} from "react-redux";
+import GridPagos from "./Components/GridPagos";
+import {
+  Container,
+  Tooltip,
+  Zoom,
+  Chip,
+  InputAdornment,
+  Card,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Breadcrumbs,
+} from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import { emphasize, withStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useDispatch, useSelector } from "react-redux";
 
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import SaveIcon from '@material-ui/icons/Save';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import SearchIcon from '@material-ui/icons/Search';
-import * as Actions from '../../../redux/actions/PagosGestorAction';
-import * as ActionMensaje from '../../../redux/actions/messageAction';
+import SaveIcon from "@material-ui/icons/Save";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import SearchIcon from "@material-ui/icons/Search";
+import * as Actions from "../../../redux/actions/PagosGestorAction";
+import * as ActionMensaje from "../../../redux/actions/messageAction";
+
+import GridTransferenciaModal from "./Components/GridTransferencia";
 
 const StyledBreadcrumb = withStyles((theme) => ({
   root: {
@@ -27,246 +43,270 @@ const StyledBreadcrumb = withStyles((theme) => ({
     height: theme.spacing(3),
     color: theme.palette.grey[800],
     fontWeight: theme.typography.fontWeightRegular,
-    '&:hover, &:focus': {
+    "&:hover, &:focus": {
       backgroundColor: theme.palette.grey[300],
     },
-    '&:active': {
+    "&:active": {
       boxShadow: theme.shadows[1],
       backgroundColor: emphasize(theme.palette.grey[300], 0.12),
     },
   },
-}))(Chip); 
+}))(Chip);
 
 const useStyles = makeStyles((theme) => ({
   etiqueta: {
     marginBottom: theme.spacing(1),
     marginTop: theme.spacing(1),
-    marginRight:theme.spacing(1),
-    paddingRight:theme.spacing(1),
-    paddingLeft:theme.spacing(1),
-   },
-   submitCargar: {                 
-    background: "#1872b8", 
-    boxShadow: '2px 4px 5px #1872b8',
-    color:'white',
-   },
-   submitSAVE: {                 
-  //  background: "#1872b8", 
-    background: "#f44336", 
-    boxShadow: '2px 4px 5px #1872b8',
-    color:'white',
-    marginLeft:theme.spacing(1),
-
-   },
-   gridContainer:{
-    paddingLeft:theme.spacing(1),
-    paddingRight:theme.spacing(1),
-    paddingTop:theme.spacing(1),
-    paddingBottom:theme.spacing(1),
+    marginRight: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
   },
-   containerCiclo:{
-    paddingLeft:theme.spacing(1),
-    paddingRight:theme.spacing(1),
-    paddingTop:theme.spacing(1),
-    paddingBottom:theme.spacing(1),
-
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center', 
-
+  submitCargar: {
+    background: "#1872b8",
+    boxShadow: "2px 4px 5px #1872b8",
+    color: "white",
   },
-  containerSave:{
-    paddingLeft:theme.spacing(1),
-    paddingRight:theme.spacing(1),
-    paddingTop:theme.spacing(1),
-    paddingBottom:theme.spacing(1),
-
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center', 
+  submitSAVE: {
+    //  background: "#1872b8",
+    background: "#f44336",
+    boxShadow: "2px 4px 5px #1872b8",
+    color: "white",
+    marginLeft: theme.spacing(1),
   },
-  containerCargar:{
-    paddingLeft:theme.spacing(1),
-    paddingRight:theme.spacing(1),
-    paddingTop:theme.spacing(1),
-    paddingBottom:theme.spacing(1),
+  gridContainer: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  containerCiclo: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
 
-    display:'flex',
-    flexDirection:'column',
-    alignContent:'center',
-    alignItems:'center',
-    justifyContent:'center',
-  }
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerSave: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
 
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerCargar: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 }));
 
-
- const Pagos =(props)=> {
+const Pagos = (props) => {
   let history = useHistory();
-  const dispatch=useDispatch();
-  const {userName, idUsuario} =useSelector((stateSelector)=>{ return stateSelector.load});
-  const {perfiles} = useSelector((stateSelector) =>{ return stateSelector.home});   
-  useEffect(()=>{  try{  
-     verificarAcceso(perfiles, props.location.state.namePagina + permiso.VISUALIZAR, history);
-     }catch (err) {  verificarAcceso(perfiles, 'none', history); }
-  },[])
-  const style= useStyles();
+  const dispatch = useDispatch();
+  const { userName, idUsuario } = useSelector((stateSelector) => {
+    return stateSelector.load;
+  });
+  const { perfiles } = useSelector((stateSelector) => {
+    return stateSelector.home;
+  });
+  useEffect(() => {
+    try {
+      verificarAcceso(
+        perfiles,
+        props.location.state.namePagina + permiso.VISUALIZAR,
+        history
+      );
+    } catch (err) {
+      verificarAcceso(perfiles, "none", history);
+    }
+  }, []);
+  const style = useStyles();
 
-  const[statusBusqueda, setStatusBusqueda]= useState(false);
-  const[ txtBusqueda, setTxtBusqueda]= useState('');
+  const [statusBusqueda, setStatusBusqueda] = useState(false);
+  const [txtBusqueda, setTxtBusqueda] = useState("");
 
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [mensajeSnackbar, setMensajeSnackbar] = useState("");
   const [tipoSnackbar, settipTSnackbar] = useState(true);
- 
 
-  const[idCiclo, setIdCiclo]= useState(0);
-  const[nameComboSeleccionado, setNameComboSeleccionado] = useState("");
-  const[idCicloSelected, setIdCicloSelected]= useState(0);
-  const[listCiclo, setListCiclo]= useState([]);
+  const [idCiclo, setIdCiclo] = useState(0);
+  const [nameComboSeleccionado, setNameComboSeleccionado] = useState("");
+  const [idCicloSelected, setIdCicloSelected] = useState(0);
+  const [listCiclo, setListCiclo] = useState([]);
 
-  const[listaComisionesAPagar, setListaComisionesAPagar]= useState([]);
+  const [listaComisionesAPagar, setListaComisionesAPagar] = useState([]);
 
-
-
-  async function cargarCiclo(userNa){      
-      let respuesta = await Actions.ObtenerCiclosPagos(userNa, dispatch);
-      console.log('lista ciclos: ',respuesta);
-      if(respuesta && respuesta.code == 0){ 
-        setListCiclo(respuesta.data);
-      }else{
-        dispatch(ActionMensaje.showMessage({ message: respuesta.message , variant: "error" }));
-      }
-   }
-   const seleccionarNombreCombo = (nombre)=>{
+  async function cargarCiclo(userNa) {
+    let respuesta = await Actions.ObtenerCiclosPagos(userNa, dispatch);
+    console.log("lista ciclos: ", respuesta);
+    if (respuesta && respuesta.code == 0) {
+      setListCiclo(respuesta.data);
+    } else {
+      dispatch(
+        ActionMensaje.showMessage({
+          message: respuesta.message,
+          variant: "error",
+        })
+      );
+    }
+  }
+  const seleccionarNombreCombo = (nombre) => {
     setNameComboSeleccionado(nombre);
-   }
-   const onChangeSelectCiclo= (e) => {
-      const texfiel = e.target.name;
-      const value = e.target.value;
-      if (texfiel === "idCiclo") {
-          setIdCiclo(value);        
+  };
+  const onChangeSelectCiclo = (e) => {
+    const texfiel = e.target.name;
+    const value = e.target.value;
+    if (texfiel === "idCiclo") {
+      setIdCiclo(value);
+    }
+    if (texfiel === "txtBusqueda") {
+      setTxtBusqueda(value);
+    }
+  };
+
+  const handleOnGetPagos = () => {
+    if (idCiclo && idCiclo !== 0) {
+      setIdCicloSelected(idCiclo);
+      cargarComisionesPagos(userName, idCiclo);
+    } else {
+      generarSnackBar(
+        "¡Debe Seleccionar un ciclo para cargar las comisiones!",
+        "warning"
+      );
+    }
+  };
+
+  async function cargarComisionesPagos(userNa, cicloId) {
+    let respuesta = await Actions.ObtenerComisionesPagos(
+      userNa,
+      cicloId,
+      dispatch
+    );
+    console.log("comisiones pagos: ", respuesta);
+    if (respuesta && respuesta.code == 0) {
+      setListaComisionesAPagar(respuesta.data);
+      setStatusBusqueda(true);
+    } else {
+      dispatch(
+        ActionMensaje.showMessage({
+          message: respuesta.message,
+          variant: "error",
+        })
+      );
+    }
+  }
+  //--navbar
+  const closeSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
+  const generarSnackBar = (mensaje, tipo) => {
+    setOpenSnackbar(true);
+    setMensajeSnackbar(mensaje);
+    settipTSnackbar(tipo);
+  };
+
+  const selecionarDetalleFrelances = () => {};
+
+  const seleccionarTipoFiltroBusqueda = (idTipoFormaPago) => {
+    filtrarComisionPorFormaPago(idTipoFormaPago);
+  };
+  async function filtrarComisionPorFormaPago(idTipoFormaPago) {
+    if (idCiclo && idCiclo !== 0) {
+      let response = await Actions.ListarFiltrada(
+        userName,
+        idCiclo,
+        idTipoFormaPago,
+        dispatch
+      );
+      console.log("busqueda por filtro", response);
+      if (response && response.code == 0) {
+        let data = response.data;
+        setListaComisionesAPagar(data.lista);
       }
-        if (texfiel === "txtBusqueda") {
-          setTxtBusqueda(value);
-       }
-   };
-
-   const handleOnGetPagos=()=>{         
-        if(idCiclo && idCiclo !== 0){  
-            setIdCicloSelected(idCiclo);     
-            cargarComisionesPagos(userName, idCiclo)
-
-        }else{
-          generarSnackBar('¡Debe Seleccionar un ciclo para cargar las comisiones!','warning')
-         /*  setOpenSnackbar(true);
-          setMensajeSnackbar('¡Debe Seleccionar un ciclo para cargar las comisiones!');
-          settipTSnackbar('warning'); */
-        }      
+    } else {
+      generarSnackBar(
+        "¡Debe Seleccionar un ciclo para cargar las comisiones!",
+        "warning"
+      );
     }
-
-    async function cargarComisionesPagos(userNa, cicloId){      
-      let respuesta = await Actions.ObtenerComisionesPagos(userNa, cicloId, dispatch);
-      console.log('comisiones pagos: ',respuesta);
-      if(respuesta && respuesta.code == 0){ 
-        setListaComisionesAPagar(respuesta.data);
-        setStatusBusqueda(true);    
-      }else{
-        dispatch(ActionMensaje.showMessage({ message: respuesta.message , variant: "error" }));
-      }
+  }
+  const buscarFreelanzer = () => {
+    if (txtBusqueda != "") {
+      buscarFrelancerPorCi();
+    } else {
+      generarSnackBar("¡Introduzca carnet de identidad!", "info");
     }
-    //--navbar
-      const closeSnackbar= (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
-        }
-        setOpenSnackbar(false);
-      };
-      const generarSnackBar =(mensaje, tipo)=>{
-        setOpenSnackbar(true);
-        setMensajeSnackbar(mensaje);
-        settipTSnackbar(tipo);
-      }
-    
-     const selecionarDetalleFrelances =() =>{
+  };
 
-     } 
-    
-    const seleccionarTipoFiltroBusqueda=(idTipoFormaPago)=>{
-      filtrarComisionPorFormaPago(idTipoFormaPago)
+  async function buscarFrelancerPorCi() {
+    let response = await Actions.buscarPorCarnetFormaPago(
+      userName,
+      idCiclo,
+      txtBusqueda,
+      dispatch
+    );
+    if (response && response.code == 0) {
+      console.log("response busca ", response);
+      let data = response.data;
+      setListaComisionesAPagar(data);
     }
-    async function filtrarComisionPorFormaPago(idTipoFormaPago){
-      if(idCiclo && idCiclo !== 0){  
-          let response= await Actions.ListarFiltrada(userName, idCiclo, idTipoFormaPago, dispatch)   
-          console.log('busqueda por filtro',response)          
-          if(response && response.code == 0){
-              let data= response.data;
-             // setPendienteFormaPago(data.pendienteFormaPago);
-              setListaComisionesAPagar(data.lista);  
-          }       
-      }else{
-             generarSnackBar('¡Debe Seleccionar un ciclo para cargar las comisiones!','warning');
-      }    
-    }
-    const buscarFreelanzer=()=>{
-          if(txtBusqueda != ""){ 
-              buscarFrelancerPorCi();
-          }else{
-              generarSnackBar('¡Introduzca carnet de identidad!','info');
-          }
-      }
-  
-      async function buscarFrelancerPorCi(){   
+  }
 
-             let response= await Actions.buscarPorCarnetFormaPago(userName, idCiclo, txtBusqueda, dispatch)               
-             if(response && response.code == 0){
-               console.log('response busca ', response);
-                 let data= response.data;
-                // setPendienteFormaPago(data.pendienteFormaPago);
-                 setListaComisionesAPagar(data);                 
-             }       
-       }
+  useEffect(() => {
+    cargarCiclo(userName);
+  }, []);
 
-   useEffect(()=>{
-      cargarCiclo(userName);
-   },[])
+const [openModalConfirm, setOpenModalConfirm] = useState(false);
 
-    const [openModalConfirm, setOpenModalConfirm] = useState(false);
-   const abrirModal = ()=> {
-    verificarConfirmarSionPay(userName,idCiclo);
-      //setOpenModalConfirm(true);
-   }
-   async function verificarConfirmarSionPay(userN, cicloId){   
-
-    let response= await Actions.verificarPagoSionPayXCiclo(userN, cicloId, dispatch)               
-      if(response && response.code == 0){           
-          setOpenModalConfirm(true);    
-      } else{
-         dispatch(ActionMensaje.showMessage({ message: response.message , variant: "error" }));
-      }      
-    }
-
-   const CloseModalConfirmacion =()=>{
-      setOpenModalConfirm(false);      
-   }
-   const confirmarModal =()=>{
+const abrirModal = ()=> {
+setOpenModalConfirm(true);
+}
+const CloseModalConfirmacion =()=>{
+    setOpenModalConfirm(false);      
+}
+const confirmarModal =()=>{
     if(idCiclo && idCiclo !== 0){  
       prosesarPagoSionPay(userName,idUsuario, idCiclo);
     }else{
       generarSnackBar('¡Debe seleccionar un ciclo para el cierre','info');
     }
-   }
-   async function prosesarPagoSionPay(userN,usuarioId, cicloId){   
+}
+async function prosesarPagoSionPay(userN,usuarioId, cicloId){   
 
     let response= await Actions.pagarComisionSionPay(userN,usuarioId, cicloId, dispatch)               
       if(response && response.code == 0){           
-           setOpenModalConfirm(false);      
-           dispatch(ActionMensaje.showMessage({ message: response.message , variant: "success" }));   
-           handleOnGetPagos();      
+setOpenModalConfirm(false);      
+dispatch(ActionMensaje.showMessage({ message: response.message , variant: "success" }));         
       } else{
-         dispatch(ActionMensaje.showMessage({ message: response.message , variant: "error" }));
+dispatch(ActionMensaje.showMessage({ message: response.message , variant: "error" }));
       }      
     }
+
+    const [openModalFullScreen, setOpenModalFullScreen] = useState(false);
+
+  const openFullScreenModal = () => {
+    setOpenModalFullScreen(true);
+  };
+
+  const closeFullScreenModal = () => {
+    setOpenModalFullScreen(false);
+  };
 
 
     return (
@@ -277,8 +317,7 @@ const useStyles = makeStyles((theme) => ({
           </Breadcrumbs>
         </div>
         <br />
-        <Typography variant="h4" gutterBottom  >
-             {'Pagos'}
+        <Typography variant="h4" gutterBottom  >{'Pagos'}
         </Typography>     
 
       <Card>
@@ -305,7 +344,7 @@ const useStyles = makeStyles((theme) => ({
                       variant="contained"
                       color="secondary"
                       className={style.submitSAVE}
-                      //onClick = {() => openFullScreenModal()}
+                      onClick = {() => openFullScreenModal()}
                     >
                       PAGAR POR TRANSFERENCIA
                     </Button>
@@ -445,7 +484,7 @@ const useStyles = makeStyles((theme) => ({
         handleCloseConfirm={confirmarModal}
         handleCloseCancel={CloseModalConfirmacion}
       />
-      {/* <GridTransferenciaModal openModalFullScreen = {openModalFullScreen} closeFullScreenModal = {closeFullScreenModal}/> */}
+      <GridTransferenciaModal openModalFullScreen = {openModalFullScreen} closeFullScreenModal = {closeFullScreenModal}/>
     </>
   );
 };
