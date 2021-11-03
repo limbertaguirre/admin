ab1da2cc29f92be8fbb548792278f8c681428f67
Marwin Germán Núñez Robles <mgnunez@gruposion.bo>
2021-11-03 16:55:10 -0400

    22/69

--- gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
+++ gestion_de_comisiones/ClientApp/src/pages/pagoComisiones/Pagos/Pagos.js
@@ -36,7 +36,11 @@ import * as Actions from '../../../redux/actions/PagosGestorAction';
 import * as ActionMensaje from '../../../redux/actions/messageAction';
 import TransferenciasDialog from './Components/TransferenciasDialog'
 
+<<<<<<< HEAD
 // import GridTransferenciaModal from "./Components/GridTransferencia";
+=======
+import GridTransferencia from "./Components/GridTransferencia";
+>>>>>>> 4209604 (Rebase final.)
 
 const StyledBreadcrumb = withStyles((theme) => ({
   root: {
@@ -238,7 +242,6 @@ const Pagos = (props) => {
       console.log("busqueda por filtro", response);
       if (response && response.code == 0) {
         let data = response.data;
-        // setPendienteFormaPago(data.pendienteFormaPago);
         setListaComisionesAPagar(data.lista);
       }
     } else {
@@ -266,7 +269,6 @@ const Pagos = (props) => {
     if (response && response.code == 0) {
       console.log("response busca ", response);
       let data = response.data;
-      // setPendienteFormaPago(data.pendienteFormaPago);
       setListaComisionesAPagar(data);
     }
   }
