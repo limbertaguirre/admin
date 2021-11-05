c739a4e270cc4166632f54049f75578521132b43
laguirre <laguirre@gruposion.bo>
2021-11-05 11:15:49 -0400

    agregando el ignore y bd en 15

--- gestion_de_comisiones/MultinivelModel/BDMultinivelContext.cs
+++ gestion_de_comisiones/MultinivelModel/BDMultinivelContext.cs
@@ -91,7 +91,7 @@ namespace gestion_de_comisiones.MultinivelModel
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=10.2.10.20;Database=BDMultinivel; User Id=sa;password=Passw0rd;");
+                optionsBuilder.UseSqlServer("Server=10.2.10.15;Database=BDMultinivel; User Id=sa;password=Passw0rd;");
             }
         }
 
