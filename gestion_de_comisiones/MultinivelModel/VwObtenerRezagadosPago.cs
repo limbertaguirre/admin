abd2860944b116e46de497efe3fd0ba8f6e17d7c
laguirre <laguirre@gruposion.bo>
2021-11-29 14:53:28 -0400

    agregando el nuevo modelo de rezagado

--- gestion_de_comisiones/MultinivelModel/VwObtenerRezagadosPago.cs
+++ gestion_de_comisiones/MultinivelModel/VwObtenerRezagadosPago.cs
@@ -5,18 +5,15 @@ using System.Collections.Generic;
 
 namespace gestion_de_comisiones.MultinivelModel
 {
-    public partial class VwObtenerInfoExcelFormatoBanco
+    public partial class VwObtenerRezagadosPago
     {
         public int? IdCiclo { get; set; }
         public int IdEmpresa { get; set; }
         public string Empresa { get; set; }
-        public int IdListaFormasPago { get; set; }
         public int IdComisionesDetalle { get; set; }
         public int IdComisionDetalleEmpresa { get; set; }
         public byte? IdEstadoComisionDetalleEmpresa { get; set; }
-        public int IdFicha { get; set; }
         public string CodigoDeCliente { get; set; }
-        public string NombreBanco { get; set; }
         public string NroDeCuenta { get; set; }
         public string NombreDeCliente { get; set; }
         public string DocDeIdentidad { get; set; }
