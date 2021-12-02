3acd656b91d7b93670531c92e39ba465fb1ce099
Eliot Humerez <ehumerez@gruposion.bo>
2021-12-02 12:12:57 -0400

    Quitando lineas comentadas

--- gestion_de_comisiones/Repository/Interfaces/IGestionPagosRezagadosRepository.cs
+++ gestion_de_comisiones/Repository/Interfaces/IGestionPagosRezagadosRepository.cs
@@ -4,19 +4,6 @@ namespace gestion_de_comisiones.Repository.Interfaces
     public interface IGestionPagosRezagadosRepository
     {
         public object GetCiclos(string usuario, int idEstadoComision, int idTipoComisionPagoComision);
-        public object GetComisionesPagos(string usuario, int idCiclo, int idEstadoComision, int idTipoComisionPagoComision);
-        /*public object GetFiltroComisionesPorFormaPago(FiltroFormaPagosInput param, int idEstadoComision, int idEstadoDetalleSifacturo, int idEstadoDetalleNoPresentaFactura, int idTipoComisionPagoComision);
-        public List<VwObtenercomisionesFormaPago> GetComisionesPorCarnetListPagos(BuscarComisionInput param, int idEstadoComision, int idEstadoDetalleSifacturo, int idEstadoDetalleNoPresentaFactura, int idTipoComisionPagoComision);
-        public bool PagarSionPayComision(PagarSionPayInput param);
-        public RespuestaSionPayModel VerificarPagoSionPayCiclo(VerificarPagoSionPayInput param, int idEstadoComision, int idEstadoDetalleSifacturo, int idEstadoDetalleNoPresentaFactura, int idTipoComisionPagoComision, int idTipoFormaPagoSionPay);
-        public dynamic handleTransferenciasEmpresas(ComisionesPagosInput param);
-        object handleDownloadFileEmpresas(DownloadFileTransferenciaInput body);
-        bool handleConfirmarPagosTransferenciasTodos(DownloadFileTransferenciaInput body);
-        GestionPagosEvent handleVerificarPagosTransferenciasTodos(DownloadFileTransferenciaInput body);
-        GestionPagosEvent handleConfirmarPagosTransferencias(ConfirmarPagosTransferenciasInput body);
-        object handleObtenerPagosTransferencias(DownloadFileTransferenciaInput body);
-        object handleRechazadosPagosTransferencias(ConfirmarPagosTransferenciasInput param);
-        public List<VwObtenercomisionesFormaPago> FiltrarComisionPagoPorTipoPago(FiltroComisionTipoPagoInput param, int idEstadoComision, int idTipoComisionPagoComision);
-        public bool VerificarSiExisteAutorizacionFormaPagoCiclo(string usuarioLogin, int idCiclo);*/
+        public object GetComisionesPagos(string usuario, int idCiclo, int idEstadoComision, int idTipoComisionPagoComision);        
     }
 }
--- gestion_de_comisiones/Servicios/Interfaces/IGestionPagosRezagadosService.cs
+++ gestion_de_comisiones/Servicios/Interfaces/IGestionPagosRezagadosService.cs
@@ -7,17 +7,5 @@ namespace gestion_de_comisiones.Servicios.Interfaces
     {
         public object GetCiclos(string usuario);
         public object GetComisionesDePagos(ComisionesPagosInput param);
-        /*public object GetFormaPagosDisponibles(FiltroFormaPagosInput param);
-        public object ListarComisionesFormaPagoPorCarnet(BuscarComisionInput param);
-        public object PagarSionPayComisionTodo(PagarSionPayInput param);
-        public object VerificarPagoSionPayCiclo(VerificarPagoSionPayInput param);
-        object handleTransferenciasEmpresas(ComisionesPagosInput param);
-        object handleDownloadFileEmpresas(DownloadFileTransferenciaInput body);
-        object handleConfirmarPagosTransferenciasTodos(DownloadFileTransferenciaInput body);
-        object handleVerificarPagosTransferenciasTodos(DownloadFileTransferenciaInput body);
-        object handleConfirmarPagosTransferencias(ConfirmarPagosTransferenciasInput param);
-        object handleObtenerPagosTransferencias(DownloadFileTransferenciaInput param);
-        object handleRechazadosPagosTransferencias(ConfirmarPagosTransferenciasInput param);
-        public object FiltrarComisionesPorTipoPago(FiltroComisionTipoPagoInput param);*/
     }
 }
