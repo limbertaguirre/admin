c7f238f5ab86af9e69a67606d09e33291ca9f3be
Eliot Humerez <ehumerez@gruposion.bo>
2021-12-03 13:52:52 -0400

    Cambiando el tipo de entrada para la verificacion de pagos rezagados

--- gestion_de_comisiones/Repository/Interfaces/IGestionPagosRezagadosRepository.cs
+++ gestion_de_comisiones/Repository/Interfaces/IGestionPagosRezagadosRepository.cs
@@ -9,6 +9,6 @@ namespace gestion_de_comisiones.Repository.Interfaces
         public object GetCiclos(string usuario, int idEstadoComision, int idTipoComisionPagoComision);
         public object GetComisionesPagos(string usuario, int idCiclo, int idEstadoComision, int idTipoComisionPagoComision);
         public dynamic handleTransferenciasEmpresas(ComisionesPagosInput param);
-        public GestionPagosRezagadosEvent handleVerificarPagosTransferenciasTodos(DownloadFileTransferenciaInput body);
+        public GestionPagosRezagadosEvent handleVerificarPagosTransferenciasTodos(ObtenerRezagadosPagosTransferenciasInput body);
     }
 }
