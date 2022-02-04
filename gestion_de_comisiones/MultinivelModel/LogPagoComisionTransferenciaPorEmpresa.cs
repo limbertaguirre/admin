946a7c4567f089e523865de1c7070c5d54292d61
Marwin Germán Núñez Robles <mgnunez@gruposion.bo>
2022-02-04 11:26:41 -0400

    Se ejecuta el script de actualización de esquema de base de datos.

---
+++ gestion_de_comisiones/MultinivelModel/LogPagoComisionTransferenciaPorEmpresa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace gestion_de_comisiones.MultinivelModel
+{
+    public partial class LogPagoComisionTransferenciaPorEmpresa
+    {
+        public int Id { get; set; }
+        public int CicloId { get; set; }
+        public int ComisionId { get; set; }
+        public int ComisionDetalleId { get; set; }
+        public int ComisionDetalleEmpresaId { get; set; }
+        public int EmpresaId { get; set; }
+        public int EmpresaIdCnx { get; set; }
+        public int FichaId { get; set; }
+        public string Ci { get; set; }
+        public string NombreCompleto { get; set; }
+        public string NroCuentaBanco { get; set; }
+        public string Banco { get; set; }
+        public decimal Monto { get; set; }
+        public string Descripcion { get; set; }
+        public string NombreSp { get; set; }
+        public int? CodigoRespSp { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaActualizacion { get; set; }
+    }
+}
